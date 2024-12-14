f9ee76e6d0e05698c9265b03cea79d71f18a0337
jaimpalepi <zaimpahlevi25@gmail.com>
2024-12-14 21:41:12 +0700

    a

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 $currency = '₹';
 $db_username = 'root';
 $db_password = '';
-$db_name = 'prakpemweb';
+$db_name = 'bombakar';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
